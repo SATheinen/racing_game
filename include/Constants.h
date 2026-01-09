eb0c3acc27c08b6654152bf0221ccf33d98ba119
Silas Theinen <silas.theinen@rwth-aachen.de.com>
2026-01-10 00:34:55 +0100

    add file with constants

---
+++ include/Constants.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Screen dimensions
+const int SCREEN_WIDTH = 960;
+const int SCREEN_HEIGHT = 640;
+
+// Rendering constants
+const float HORIZON_Y = 320.0f;
+const float CAMERA_HEIGHT = 1000.0f;
+
+// Track constants
+const int NUM_SEGMENTS = 300;
+const float SEGMENT_LENGTH = 200.0f;
+const float TRACK_LENGTH = NUM_SEGMENTS * SEGMENT_LENGTH;
